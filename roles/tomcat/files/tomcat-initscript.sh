0847946142f07c06993f8c07014137bfd51b9d4a
elalaouim <elalaouim@ALT8P009.fr.jcdecaux.org>
2016-08-26 12:00:07 +0200

    modify JDK version

--- roles/tomcat/files/tomcat-initscript.sh
+++ roles/tomcat/files/tomcat-initscript.sh
@@ -15,7 +15,7 @@
 #
  
 #Location of JAVA_HOME (bin files)
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
  
 #Add Java binary files to PATH
 export PATH=$JAVA_HOME/bin:$PATH
